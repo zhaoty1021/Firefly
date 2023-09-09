490bc5b056d6a032a1f61caff7637f2e54f2b852
tyrone <zhao8741@qq.com>
2023-09-09 21:58:39 +0800

    优化模块

---
+++ Firefly-client/src/main/java/com/yingxiu/client/FireflyClientApplication.java
@@ -0,0 +1,13 @@
+package com.yingxiu.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireflyClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FireflyClientApplication.class, args);
+    }
+
+}
---
+++ Firefly-client/src/test/java/com/yingxiu/client/FireflyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yingxiu.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireflyClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
